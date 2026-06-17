d1a1d91734cb1f5635234c4c069cc658bb12858d
fivawyr <finn.github@gmail.com>
2026-06-17 23:06:57 +0200

    SDL init

--- code.c
+++ code.c
@@ -1,3 +1,4 @@
+#include <SDL_events.h>
 #include <SDL_timer.h>
 #include <SDL_video.h>
 #include <stdio.h>
@@ -9,5 +10,12 @@
 int main() {
     SDL_Init(SDL_INIT_VIDEO);
     SDL_CreateWindow("Mandelbrot Simulation", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, WIDTH, HEIGHT, 0);
-    SDL_Delay(3000);
+    SDL_Delay(3002);
+    
+    int application_running = 1;
+    while (application_running) {
+        SDL_Event event;
+        SDL_PollEvent(&event);
+
+    } 
 }
